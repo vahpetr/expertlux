3136380c521a15d7ca887c7e03afc5822fdbc461
Pyotr Vasilev <vahpetr@gmail.com>
2016-04-23 23:02:26 +0500

    fix build process again...

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-npm install
-bower install
-dnu restore
-dnu build
-gulp clean
-gulp release
+npm install && \
+/root/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu restore && \
+/root/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu build && \
+gulp clean && \
+gulp release && \
 /etc/init.d/supervisor force-stop && \
 /etc/init.d/supervisor stop && \
 /etc/init.d/supervisor start

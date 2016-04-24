dd08b101c7f47c4f891e7dc6b00d90c4e04e9be5
Pyotr Vasilev <vahpetr@gmail.com>
2016-04-24 13:36:57 +0500

    building process changes again

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #rm -rf node_modules && \
-npm install --no-bin-links --no-optional && \
-bower install --allow-root && \
-~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu restore && \
-~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu build && \
-gulp clean && \
-gulp release && \
+npm install --no-bin-links --no-optional
+bower install --allow-root
+~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu restore
+~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu build
+gulp clean
+gulp release
 /etc/init.d/supervisor force-stop && \
 /etc/init.d/supervisor stop && \
 /etc/init.d/supervisor start

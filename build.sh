96b939fdff2885519308b92513859c46c8197a7c
Pyotr Vasilev <vahpetr@gmail.com>
2016-06-02 17:38:15 +0500

    * style fix * build script test fix

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 npm install --no-bin-links --no-optional
 bower install --allow-root
 ~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu restore
-~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu build
-gulp clean
+~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu build && \
+gulp clean && \
 gulp release
 /etc/init.d/supervisor force-stop && \
 /etc/init.d/supervisor stop && \

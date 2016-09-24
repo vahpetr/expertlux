ad15c5b7c6870b4c48426f0da337bcc807d413c4
Васильев Петр Сергеевич <vasilev@proitr.ru>
2016-09-25 03:33:37 +0500

    changed build

--- startup.sh
+++ startup.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-export HOME=/root
-
-dnu publish --configuration Release
-
 if [ -f "/run/expertlux-live.sock" ]; then
   rm "/run/expertlux-live.sock"
 fi
 
-cd ~www/expertlux/bin/output/approot
+cd /var/www/expertlux/bin/output/wwwroot
 bash web-live \
 --configuration Release Microsoft.AspNet.Server.Kestrel \
 --server.urls http://unix:/run/expertlux-live.sock

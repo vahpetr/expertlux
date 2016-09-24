c88ed9bd26173a4629f85d9f40223a50c07525ae
Васильев Петр Сергеевич <vasilev@proitr.ru>
2016-09-25 00:37:53 +0500

    changed build process

--- build.sh
+++ build.sh
@@ -12,11 +12,10 @@ npm install --no-bin-links --no-optional
 #sorry for root
 node_modules/.bin/bower install --config.interactive=false -s --allow-root
 
-~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu restore
-~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu build && \
+gulp clean && gulp release && \
 
-gulp clean && \
-gulp release
+~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu restore && \
+~/.dnx/runtimes/dnx-mono.1.0.0-rc1-update2/bin/dnu build && \
 
 /etc/init.d/supervisor force-stop && \
 /etc/init.d/supervisor stop && \
